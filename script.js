1d26f361c532b3c90c13150228f94607fd19d7bb
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-05-17 14:50:51 +0300

    Done function multiply

---
+++ script.js
@@ -0,0 +1,9 @@
+//  Дан массив из чисел
+// Сделайте из него массив, состоящий из квадратов этих чисел.
+// const array = [1, 2, 3, 4, 5];
+
+// function multiply(arr) {
+//     return array.map(item => item * item);
+// }
+// console.log(multiply(array));
+
